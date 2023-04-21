91fbcefaa8600790ebccc9d89d0d53b3ef9e5fbc
Banut Raul <banut.raul@yahoo.com>
2023-04-21 19:55:57 +0300

    Update: Add View for main page for MemorizeGame

---
+++ Memorize Game/MemorizeGame/MemorizeGame/View/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by Raul Banut on 29.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
+            }
+            .foregroundColor(.red)
+            .padding(.horizontal)
+            
+            //            HStack(spacing: 0) {
+            //                GenericButton {
+            //                    if emojiCount < vehicles.count - 1 {
+            //                        emojiCount += 1
+            //                    }
+            //                } label: {
+            //                    Image(systemName: "plus.circle")
+            //                }
+            //
+            //            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let game = EmojiMemoryGame()
+        
+        ContentView(viewModel: game)
+    }
+}
