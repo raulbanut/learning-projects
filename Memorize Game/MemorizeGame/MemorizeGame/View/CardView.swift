09233186d54fe748e7ddb6fa2d84b6cf52bdfe22
Banut Raul <banut.raul@yahoo.com>
2023-04-21 19:55:33 +0300

    Update: Add View for Cards for MemorizeGame

---
+++ Memorize Game/MemorizeGame/MemorizeGame/View/CardView.swift
@@ -0,0 +1,36 @@
+//
+//  CardView.swift
+//  MemorizeGame
+//
+//  Created by Raul Banut on 29.03.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            let roundedShape = RoundedRectangle(cornerRadius: 20)
+            if card.isFacedUp {
+                roundedShape.fill().foregroundColor(.white)
+                roundedShape.strokeBorder(lineWidth: 3)
+                Text(card.content)
+                    .font(.largeTitle)
+            } else {
+                roundedShape.fill()
+            }
+        }
+        .frame(maxHeight: .infinity)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack {
+            CardView(card: MemoryGame<String>.Card.init(content: "🚗", id: 1))
+            CardView(card: MemoryGame<String>.Card.init(content: "🚌", id: 1))
+        }
+    }
+}
