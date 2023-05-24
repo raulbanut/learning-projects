4bf2d0425ad906f7caba98588180b51ec3d25bb7
Banut Raul <banut.raul@yahoo.com>
2023-05-24 15:32:36 +0300

    Update: Start to work on MoneyTracker project, first init and add firebase

---
+++ MoneyTracker/MoneyTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MoneyTracker
+//
+//  Created by Banut Raul on 23.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyTracker/MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,28 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Banut Raul on 23.05.2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct MoneyTrackerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
