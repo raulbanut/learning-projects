c4513557f8de820a3fbae181a4703af57619bcd7
Banut Raul <banut.raul@yahoo.com>
2023-05-30 11:31:15 +0300

    Add: Add Launch Screen and colors

---
+++ MoneyTracker/MoneyTracker/Colors+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Colors+Extensions.swift
+//  MoneyTracker
+//
+//  Created by Banut Raul on 30.05.2023.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+extension UIColor {
+    
+    static let puertoRico = UIColor(named: "PuertoRico")!
+}
+
+extension Color {
+   
+    static let puertoRico = Color("PuertoRico")
+}
+
